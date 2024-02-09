15996d8a3de8a2f1ac39b5eac108039b2f6eef8f
SaimaS <saima.salonen@gmail.com>
2024-02-09 12:52:28 +0200

    07 1.5 kurssista ja sen osista tehty yksi javascript olio. Hienosäädetty komponentteja, jotta koodi toimii yhtenä oliona. 1.5 valmis.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 const Header = (props) => {
-  console.log(`Header propsi: ${props.course}`);
+  console.log(`Header propsi: ${props.course.name}`);
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 };
@@ -47,27 +47,28 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundimentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundimentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
